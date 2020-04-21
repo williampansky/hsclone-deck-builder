2c3bae428fd05b36317b6a0b34b420b5993a3645
William Pansky <williampansky@gmail.com>
2020-04-20 22:24:36 -0500

    integrated redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 
 if (process.env.NODE_ENV !== 'production' && module.hot) {
   module.hot.accept('./App', () => {
     const App = require('./App').default;
-    ReactDOM.render(<App />, document.getElementById('root'));
+    ReactDOM.render(
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>,
+      document.getElementById('root')
+    );
   });
 }
 
