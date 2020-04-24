770aec5f80fcb8d00b064f67ffa27569de9d5f89
William Pansky <williampansky@gmail.com>
2020-04-24 14:33:33 -0500

    swapped renders

--- src/index.js
+++ src/index.js
@@ -7,15 +7,6 @@ import store from 'store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
-
 if (process.env.NODE_ENV !== 'production' && module.hot) {
   module.hot.accept('./App', () => {
     const App = require('./App').default;
@@ -29,6 +20,15 @@ if (process.env.NODE_ENV !== 'production' && module.hot) {
     );
   });
 }
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
