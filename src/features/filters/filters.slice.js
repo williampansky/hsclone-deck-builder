db7563886b1c55531890517adf9bdd1d2a2ce072
William Pansky <williampansky@gmail.com>
2020-04-27 10:48:20 -0500

    updates to art assets

--- src/features/filters/filters.slice.js
+++ src/features/filters/filters.slice.js
@@ -18,6 +18,18 @@ let initialState = {
     { _order: 7, name: replaceConstant(CARDCLASS[7]), value: CARDCLASS[7] },
     { _order: 8, name: replaceConstant(CARDCLASS[8]), value: CARDCLASS[8] },
     { _order: 9, name: replaceConstant(CARDCLASS[9]), value: CARDCLASS[9] }
+  ],
+  availableCardRarities: [
+    { _order: 0, name: replaceConstant(CARDCLASS[0]), value: CARDCLASS[0] },
+    { _order: 1, name: replaceConstant(CARDCLASS[1]), value: CARDCLASS[1] },
+    { _order: 2, name: replaceConstant(CARDCLASS[2]), value: CARDCLASS[2] },
+    { _order: 3, name: replaceConstant(CARDCLASS[3]), value: CARDCLASS[3] },
+    { _order: 4, name: replaceConstant(CARDCLASS[4]), value: CARDCLASS[4] },
+    { _order: 5, name: replaceConstant(CARDCLASS[5]), value: CARDCLASS[5] },
+    { _order: 6, name: replaceConstant(CARDCLASS[6]), value: CARDCLASS[6] },
+    { _order: 7, name: replaceConstant(CARDCLASS[7]), value: CARDCLASS[7] },
+    { _order: 8, name: replaceConstant(CARDCLASS[8]), value: CARDCLASS[8] },
+    { _order: 9, name: replaceConstant(CARDCLASS[9]), value: CARDCLASS[9] }
   ]
 };
 
